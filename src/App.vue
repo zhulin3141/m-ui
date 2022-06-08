db100ebd4924e8a65e199eaba90e2b2c0c9873b7
zhulin3141 <zhulin3141@gmail.com>
2022-06-08 15:13:57 +0800

    add input components

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
       <MButton type="danger" size="sm" disabled>禁止</MButton>
       <MButton type="text" size="sm" @click="handleClick">文字</MButton>
     </div>
-    
+
     <div id="btn-group-demo">
       <MButtonGroup>
         <MButton>左</MButton>
@@ -16,29 +16,50 @@
         <MButton>右</MButton>
       </MButtonGroup>
     </div>
+
+    <div id="btn-input">
+      <MInput placeholder="请输入数据" />
+      <MInput value="我是谁" placeholder="请输入数据" @blur="showInputData" />
+      <MInput type="password" placeholder="请输入密码" />
+      <MInput value="333333333" disabled placeholder="请输入数据" />
+      <MInput value="55555555555" readonly placeholder="请输入数据" />
+
+      <div class="m-row">
+        <MInput placeholder="请输入数据">
+          <template slot="before">http://</template>
+          <template slot="after">.com</template>
+        </MInput>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import MButton from "./components/button/src/button";
 import MButtonGroup from "./components/button/src/button-group";
+import MInput from "./components/input/src/input";
 
 export default {
   name: "App",
   components: {
     MButton,
     MButtonGroup,
+    MInput,
   },
   created() {},
   methods: {
     handleClick(e) {
       console.log(e);
     },
+
+    showInputData() {
+      console.log(this);
+    },
   },
 };
 </script>
 
-<style>
+<style lang="less">
 html,
 body {
   margin: 0;
@@ -49,5 +70,14 @@ body {
 }
 #btn-demo .m-button {
   margin: 3px;
+}
+.m-row {
+  width: 100%;
+  .m-input-wrapper {
+    display: inline-table;
+    .m-input{
+      border-radius: 0;
+    }
+  }
 }
 </style>
---
+++ src/components/input/src/input.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="m-input-wrapper">
+    <!-- 前置元素 -->
+    <div class="m-input-group__before" v-if="$slots.before">
+      <slot name="before"></slot>
+    </div>
+
+    <input
+      class="m-input"
+      :value="value"
+      :type="type"
+      :disabled="disabled"
+      :readonly="readonly"
+      :placeholder="placeholder"
+      @input="handleInput"
+      @change="handleChange"
+      @focus="handleFocus"
+      @blur="handleBlur"
+    />
+
+    <!-- 后置元素 -->
+    <div class="m-input-group__after" v-if="$slots.after">
+      <slot name="after"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "m-input",
+  props: {
+    type: {
+      type: String,
+      required: false,
+      default: "text",
+    },
+    value: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    readonly: {
+      type: Boolean,
+      require: false,
+      default: false,
+    },
+    placeholder: {
+      type: String,
+      required: false,
+      default: "",
+    },
+  },
+  data() {
+    return {};
+  },
+
+  computed: {},
+
+  mounted() {},
+
+  methods: {
+    handleInput(event) {
+      this.$emit("input", event.target.value);
+    },
+    handleChange(event) {
+      this.$emit("change", event.target.value);
+    },
+    handleFocus(event) {
+      this.focused = true;
+      this.$emit("focus", event);
+    },
+    handleBlur(event) {
+      this.focused = false;
+      this.$emit("blur", event);
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+@import "../../../assets/style/var";
+
+.m-input-wrapper {
+  & .m-input-group__before,
+  & .m-input-group__after {
+    display: table-cell;
+    .base-font();
+    padding: 0.8em 1em;
+    border: 1px solid @border-color;
+    border-radius: @border-radius;
+    white-space: nowrap;
+  }
+  & .m-input-group__before {
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+  & .m-input-group__after {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+    border-left: 0;
+  }
+
+  & .m-input {
+    display: table-cell;
+    box-sizing: border-box;
+    padding: @padding @padding-bigger;
+    border-radius: @border-radius;
+    border: @input-border solid @border-color;
+    margin-left: -1px;
+
+    &:hover {
+      border-color: @border-color-hover;
+    }
+    &:focus {
+      outline: none;
+      border-color: @border-color-focus;
+      box-shadow: 0 0 1px 1px @box-shadow-color;
+    }
+    &[disabled],
+    &[readonly] {
+      cursor: not-allowed;
+      border-color: @border-color-disabled;
+      background-color: @bg-color-disabled;
+      color: @color-disabled;
+    }
+    &::placeholder {
+      color: @color-placeholder;
+    }
+  }
+}
+</style>
