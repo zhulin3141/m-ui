cbbf88291688ed86f3c4f07406251fde23587180
zhulin3141 <zhulin3141@gmail.com>
2022-06-10 11:05:28 +0800

    add accordion transtion

--- src/components/accordion/src/accordion-item.vue
+++ src/components/accordion/src/accordion-item.vue
@@ -3,9 +3,11 @@
     <div class="m-accordion-item-title" @click="toggle" :class="dynamicClass">
       {{ title }}
     </div>
-    <div class="m-accordion-item-content" v-if="show">
-      <slot></slot>
-    </div>
+    <transition name="collapse-transition">
+      <div class="m-accordion-item-content" v-if="show">
+        <slot></slot>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -61,25 +63,35 @@ export default {
   margin-left: -1px;
   margin-right: -1px;
   margin-top: -1px;
-  .m-accordion-item-title {
+  &-title {
     .base-font(14px, 18px, #303133);
     border: 1px solid @border-color;
     padding: 1rem 1.25rem;
     cursor: pointer;
   }
-  &:first-of-type > .m-accordion-item-title {
+  &:first-of-type > &-title {
     .border-top-radius();
   }
-  &:last-of-type > .m-accordion-item-title {
+  &:last-of-type > &-title {
     .border-bottom-radius();
     margin-bottom: -1px;
   }
-  > .m-accordion-item-content {
+  &-content {
     padding: 1rem 1.25rem;
   }
   .active-accordion-item {
     border-bottom-left-radius: 0;
     border-bottom-right-radius: 0;
+  }
+}
+
+.collapse-transition{
+  &-enter-active, &-leave-active{
+    overflow: hidden;
+    transition: all 0.35s ease;
+  }
+  &-enter-from, &-leave-to{
+    overflow: hidden;
   }
 }
 </style>
