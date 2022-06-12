0f970d627fc382c0a4778b986311fb50a6ab1898
zhulin3141 <zhulin3141@gmail.com>
2022-06-13 01:15:26 +0800

    add table component

---
+++ src/components/table/src/table-column.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "m-table-column",
+  props: {
+    row: Object,
+    column: Object,
+    index: Number,
+  },
+  render(h) {
+    return h('div', this.$slots.default)
+  },
+};
+</script>
---
+++ src/components/table/src/table.vue
@@ -0,0 +1,70 @@
+<template>
+    <table class="m-table" :border="this.border ? 1: 0" :class="{'table-striped': this.striped}">
+        <thead>
+            <tr>
+                <th v-for="column in columns" :key="column.prop">
+                    {{ column.label }}
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(row, index) in rows" :key="index">
+                <td v-for="(v, k) in row" :key="k">{{ v }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+
+export default {
+    name: "m-table",
+    props: {
+        data: {
+            type: Array,
+            default: () => [],
+        },
+        striped: {
+            type: Boolean,
+            default: false,
+        },
+        border: {
+            type: Boolean,
+            default: false,
+        }
+    },
+    computed: {
+        columns() {
+            return this.$slots.default.map(({data: {attrs}}) => ({
+                prop: attrs.prop,
+                label: attrs.label,
+            }))
+        },
+        rows() {
+            return this.data.map(item=>{
+                return this.columns.reduce((prev, {prop})=>{
+                    prev[prop] = item[prop];
+                    return prev;
+                }, {})
+            })
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+@import "../../../assets/style/var";
+
+.m-table {
+    width: 100%;
+    margin-bottom: 1rem;
+    color: #212529;
+    vertical-align: top;
+    border-color: @border-color;
+    border-collapse: collapse;
+    text-align: left;
+    &.table-striped > tbody > tr:hover {
+        background: @border-color-hover;
+    }
+}
+</style>
