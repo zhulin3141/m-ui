0326ef6684d933c75cee131806f75e460d1fb24a
zhulin3141 <zhulin3141@gmail.com>
2022-06-14 10:43:58 +0800

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git push -f https://zhulin3141:<TOKEN>@github.com/zhulin3141/m-ui.git master:gh-pages
+git push -f https://zhulin3141:<TOKEN>@github.com/zhulin3141/m-ui.git master:gh-pages
 
 cd -
